e104d323744fc78dc0b370903247078aee758d2e
Ryan <rquinn.zk@gmail.com>
2011-02-01 16:23:28 -0700

    added email smtp settings to config file, made things a little more friendly to update

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,9 +1,10 @@
 class Notifier < ActionMailer::Base
-  default :from => APP_CONFIG["password_resets_from_address"]
+  default :from => APP_CONFIG["email_from"]
   default_url_options[:host] = APP_CONFIG["cas_user_management_url"]  
   
   def password_reset_instructions(user)  
 	subject       "Password Reset Instructions"  
+	from		  APP_CONFIG["email_from"]
 	recipients    user.email  
 	sent_on       Time.now  
 	body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
--- config/initializers/load_config_and_rubycas.rb
+++ config/initializers/load_config_and_rubycas.rb
@@ -6,3 +6,12 @@ APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")[RAILS_ENV]
 CASClient::Frameworks::Rails::Filter.configure(
     :cas_base_url => APP_CONFIG["cas_base_url"]
   )
+  
+ActionMailer::Base.smtp_settings = {
+  :address => APP_CONFIG["email_smtp_address"],
+  :port => APP_CONFIG["email_port"],
+  :domain => APP_CONFIG["email_domain"],
+  :authentication => "plain",
+  :user_name => APP_CONFIG["email_username"],
+  :password => APP_CONFIG["email_password"]
+}
